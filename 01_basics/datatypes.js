4c76e945adaba71e29abc159cc5385f18a43d812
Shibam19 <shibamde1@gmail.com>
2025-01-09 19:42:18 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+console.log(3 + 3) // code readability should be high
+
+console.log("Hitesh")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value =>nothing no value
+// undefined => undefined
+// symbol => stores unique random value
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+console.table([name,age,isLoggedIn,state])
