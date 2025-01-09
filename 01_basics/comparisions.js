d93c6a5cca38ee65d76e30966443020935b1a7bd
Shibam19 <shibamde1@gmail.com>
2025-01-09 21:31:05 +0000

    comparision

---
+++ 01_basics/comparisions.js
@@ -0,0 +1,20 @@
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+
+console.log("2" > 1); // true
+console.log("02" > 1); //true
+
+console.log(null > 0); //false
+console.log(null == 0); //false
+console.log(null >= 0); //true
+
+console.log(undefined == 0);
+console.log(undefined > 0);// false everycase
+console.log(undefined < 0);
+
+// === check values as well as data type
+console.log("2"=== 2);
