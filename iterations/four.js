b8db326c47180f118042525da9df50e2efbba7bf
Shibam19 <shibamde1@gmail.com>
2025-01-12 11:22:05 +0000

    iterations

---
+++ iterations/four.js
@@ -0,0 +1,19 @@
+// for in
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+   console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    console.log(programming[key]);
+    console.log(key);  
+}
+// for in doesn't iterate in map
---
+++ iterations/three.js
@@ -0,0 +1,33 @@
+// for of
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const index of arr) {
+    console.log(index); 
+}
+
+const greetings = "Hello world!"
+for (const greet of greetings){
+    if(greet==" ") continue
+    console.log(`Each char is ${greet}`)
+}
+
+// Maps
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+console.log(map);
+
+for (const [key,value] of map) {
+    console.log(key ,":-" ,value); 
+}
+
+
+
